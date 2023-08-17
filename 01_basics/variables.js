401fdf7b8b51d19f6f8200bda1e6f286792b179e
Arshad Malik <130913111+arshad-malik@users.noreply.github.com>
2023-08-17 20:21:04 +0000

    Write about variables in details

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,1 +1,8 @@
-console.log("arshad");
+const accountId = 144553;
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountPassword = "43920"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+// always use let keywork 
