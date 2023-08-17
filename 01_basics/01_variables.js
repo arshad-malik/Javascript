d45984ae4d09e75aa77ffc447a200e6bd294a8b6
Arshad Malik <130913111+arshad-malik@users.noreply.github.com>
2023-08-17 20:34:40 +0000

    Change The file name

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553;
+let accountEmail = "arshad@google.com"
+var accountPassword = "12345"
+accountCity = "Dhaka"
+
+accountPassword = "43920"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+// always use let keywork 
